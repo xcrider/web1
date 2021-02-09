397a56548594fd584d0d6a3be0bf7123cb99d727
Karol Zawada <k.zawadaa@gmail.com>
2021-02-09 21:55:07 +0100

    Add flask project strcture

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/parent', methods=['GET'])
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html")
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect("/index")
